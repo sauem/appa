50a48814f493e6f30a1b5d6606e9a26bf1d92abd
Nguyễn Phương Nam <nam@gmail.com>
2020-03-20 09:18:12 +0700

    new3

--- wp-content/themes/appa/inc/appa.php
+++ wp-content/themes/appa/inc/appa.php
@@ -1,9 +1,29 @@
 <?php
-function simage($url , $size = []){
-    $media_url = str_replace(home_url('wp-content/uploads') . '/','',$url);
-    if(!isset($size[0]) || !isset($size[1])){
-        return home_url('wp-content/uploads/').$media_url;
+function simage($url, $size = [])
+{
+    $media_url = str_replace(home_url('wp-content/uploads') . '/', '', $url);
+    if (!isset($size[0]) || !isset($size[1])) {
+        return home_url('wp-content/uploads/') . $media_url;
     }
-    return  home_url('wp-content/uploads/vthumb.php?src='.$media_url.'&size='.$size[0].'x'.$size[1].'&zoom=1&q=90');
+    return home_url('wp-content/uploads/vthumb.php?src=' . $media_url . '&size=' . $size[0] . 'x' . $size[1] . '&zoom=1&q=90');
+}
+
+function printfs($data)
+{
+    echo "<pre>";
+    var_dump($data);
+    die;
+}
+
+
+function list1()
+{
+
+    $category = get_the_category();
+    foreach ($category as $term_id) {
+        $ids[] = $term_id->term_id;
+    }
+    return $ids;
+
 }
 
